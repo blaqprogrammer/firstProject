e41c9f34a1d05ba65b5fca94244565f6b93a1b4a
Blaqsmith <idionga2@gmail.com>
2023-07-07 20:00:27 +0100

    pushed to new branch

--- app.js
+++ app.js
@@ -1,60 +1,79 @@
+let slideIndex = 0;
+showSlides();
 
-        // Set the date we're counting down to
-        var countDownDate = new Date("July 7, 2023 15:37:25").getTime();
-        
-        // Update the count down every 1 second
-        var x = setInterval(function() {
-        
-          // Get today's date and time
-          var now = new Date().getTime();
-          console.log(Date());
-        
-          // Find the distance between now and the count down date
-          var distance = countDownDate - now;
-        
-          // Time calculations for days, hours, minutes and seconds
-          var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-          var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-          var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-          var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-        
-          // Display the result in the element with id="demo"
-          document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-          + minutes + "m " + seconds + "s ";
-        
-          // If the count down is finished, write some text
-          if (distance < 0) {
-            clearInterval(x);
-            document.getElementById("demo").innerHTML = "EXPIRED";
-          }
-        }, 1000);
-
-
-            function reveal() {
-  var reveals = document.querySelectorAll(".reveal");
-
-  for (var i = 0; i < reveals.length; i++) {
-    var windowHeight = window.innerHeight;
-    var elementTop = reveals[i].getBoundingClientRect().top;
-    var elementVisible = 150;
-
-    if (elementTop < windowHeight - elementVisible) {
-      reveals[i].classList.add("active");
-    } else {
-      reveals[i].classList.remove("active");
-    }
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
   }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+
+  slides[slideIndex-1].style.display = "block";  
+  setTimeout(showSlides, 4000); // Change image every 3 seconds
 }
 
-window.addEventListener("scroll", reveal);
 
-                const mobileBtn = document.getElementById('mobile-cta')
-                nav = document.querySelector('nav')
-                mobileBtnExit = document.getElementById('mobile-exit');
 
-                mobileBtn.addEventListener('click', ( ) => {
-                    nav.classList.add('menu-btn');
-                })
-                mobileBtnExit.addEventListener('click', ( ) => {
-                    nav.classList.remove('menu-btn');
-                })
+
+
+//         // Set the date we're counting down to
+//         var countDownDate = new Date("July 7, 2023 15:37:25").getTime();
+        
+//         // Update the count down every 1 second
+//         var x = setInterval(function() {
+        
+//           // Get today's date and time
+//           var now = new Date().getTime();
+//           console.log(Date());
+        
+//           // Find the distance between now and the count down date
+//           var distance = countDownDate - now;
+        
+//           // Time calculations for days, hours, minutes and seconds
+//           var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+//           var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+//           var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+//           var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        
+//           // Display the result in the element with id="demo"
+//           document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+//           + minutes + "m " + seconds + "s ";
+        
+//           // If the count down is finished, write some text
+//           if (distance < 0) {
+//             clearInterval(x);
+//             document.getElementById("demo").innerHTML = "EXPIRED";
+//           }
+//         }, 1000);
+
+
+//             function reveal() {
+//   var reveals = document.querySelectorAll(".reveal");
+
+//   for (var i = 0; i < reveals.length; i++) {
+//     var windowHeight = window.innerHeight;
+//     var elementTop = reveals[i].getBoundingClientRect().top;
+//     var elementVisible = 150;
+
+//     if (elementTop < windowHeight - elementVisible) {
+//       reveals[i].classList.add("active");
+//     } else {
+//       reveals[i].classList.remove("active");
+//     }
+//   }
+// }
+
+// window.addEventListener("scroll", reveal);
+
+//                 const mobileBtn = document.getElementById('mobile-cta')
+//                 nav = document.querySelector('nav')
+//                 mobileBtnExit = document.getElementById('mobile-exit');
+
+//                 mobileBtn.addEventListener('click', ( ) => {
+//                     nav.classList.add('menu-btn');
+//                 })
+//                 mobileBtnExit.addEventListener('click', ( ) => {
+//                     nav.classList.remove('menu-btn');
+//                 })
